e577966badadbb80fb6d38c0269889200b8fa395
Marina Hany Eshak <140326911+MarinaEsac@users.noreply.github.com>
2025-12-27 15:34:38 +0200

    changes

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@
   ["181"],
   {
     12372: function (e, t, n) {
-      var i = {
+       var i = {
         "./af": "44445",
         "./af.js": "44445",
         "./ar": "8779",
@@ -13,7 +13,6 @@
         "./ar-ly": "18449",
         "./ar-ly.js": "18449",
         "./ar-ma": "30721",
-        "./ar-ma.js": "30721",
         "./ar-sa": "39707",
         "./ar-sa.js": "39707",
         "./ar-tn": "79625",
@@ -36061,179 +36060,390 @@
     2292: function () {},
   },
 ]);
-Explain(() => {
-  var e = {
-      59799: function (e, r, t) {
-        t(9461),
-          t(27624),
-          t(30286),
-          t(8334),
-          t(12338),
-          t(93695),
-          t(60322),
-          t(40941),
-          t(65134),
-          t(27527),
-          t(41655),
-          t(64054),
-          t(67103);
-      },
-    },
-    r = {};
-  function t(n) {
-    var o = r[n];
-    if (void 0 !== o) return o.exports;
-    var u = (r[n] = { id: n, loaded: !1, exports: {} });
-    return e[n].call(u.exports, u, u.exports, t), (u.loaded = !0), u.exports;
-  }
-  (t.m = e),
-    (() => {
-      var e,
-        r = Object.getPrototypeOf
-          ? function (e) {
-              return Object.getPrototypeOf(e);
-            }
-          : function (e) {
-              return e.__proto__;
-            };
-      t.t = function (n, o) {
-        if (
-          (1 & o && (n = this(n)),
-          8 & o ||
-            ("object" == typeof n &&
-              n &&
-              ((4 & o && n.__esModule) ||
-                (16 & o && "function" == typeof n.then))))
-        )
-          return n;
-        var u = Object.create(null);
-        t.r(u);
-        var i = {};
-        e = e || [null, r({}), r([]), r(r)];
-        for (
-          var f = 2 & o && n;
-          "object" == typeof f && !~e.indexOf(f);
-          f = r(f)
-        )
-          Object.getOwnPropertyNames(f).forEach(function (e) {
-            i[e] = function () {
-              return n[e];
-            };
-          });
-        return (
-          (i.default = function () {
-            return n;
-          }),
-          t.d(u, i),
-          u
-        );
-      };
-    })(),
-    (t.d = function (e, r) {
-      for (var n in r)
-        t.o(r, n) &&
-          !t.o(e, n) &&
-          Object.defineProperty(e, n, { enumerable: !0, get: r[n] });
-    }),
-    (t.hmd = function (e) {
-      return (
-        !(e = Object.create(e)).children && (e.children = []),
-        Object.defineProperty(e, "exports", {
-          enumerable: !0,
-          set: function () {
-            throw Error(
-              "ES Modules may not assign module.exports or exports.*, Use ESM export syntax, instead: " +
-                e.id
-            );
-          },
-        }),
-        e
-      );
-    }),
-    (t.g = (function () {
-      if ("object" == typeof globalThis) return globalThis;
-      try {
-        return this || Function("return this")();
-      } catch (e) {
-        if ("object" == typeof window) return window;
-      }
-    })()),
-    (t.o = function (e, r) {
-      return Object.prototype.hasOwnProperty.call(e, r);
-    }),
-    (t.r = function (e) {
-      "undefined" != typeof Symbol &&
-        Symbol.toStringTag &&
-        Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }),
-        Object.defineProperty(e, "__esModule", { value: !0 });
-    }),
-    (t.nmd = function (e) {
-      return (e.paths = []), !e.children && (e.children = []), e;
-    }),
-    (() => {
-      var e = [];
-      t.O = function (r, n, o, u) {
-        if (n) {
-          u = u || 0;
-          for (var i = e.length; i > 0 && e[i - 1][2] > u; i--) e[i] = e[i - 1];
-          e[i] = [n, o, u];
-          return;
-        }
-        for (var f = 1 / 0, i = 0; i < e.length; i++) {
-          for (
-            var n = e[i][0], o = e[i][1], u = e[i][2], c = !0, a = 0;
-            a < n.length;
-            a++
-          )
-            (!1 & u || f >= u) &&
-            Object.keys(t.O).every(function (e) {
-              return t.O[e](n[a]);
-            })
-              ? n.splice(a--, 1)
-              : ((c = !1), u < f && (f = u));
-          if (c) {
-            e.splice(i--, 1);
-            var l = o();
-            void 0 !== l && (r = l);
-          }
-        }
-        return r;
-      };
-    })(),
-    (t.rv = function () {
-      return "1.1.8";
-    }),
-    (() => {
-      var e = { 466: 0 };
-      t.O.j = function (r) {
-        return 0 === e[r];
-      };
-      var r = function (r, n) {
-          var o = n[0],
-            u = n[1],
-            i = n[2],
-            f,
-            c,
-            a = 0;
-          if (
-            o.some(function (r) {
-              return 0 !== e[r];
-            })
-          ) {
-            for (f in u) t.o(u, f) && (t.m[f] = u[f]);
-            if (i) var l = i(t);
-          }
-          for (r && r(n); a < o.length; a++)
-            (c = o[a]), t.o(e, c) && e[c] && e[c][0](), (e[c] = 0);
-          return t.O(l);
-        },
-        n = (self.webpackChunk = self.webpackChunk || []);
-      n.forEach(r.bind(null, 0)), (n.push = r.bind(null, n.push.bind(n)));
-    })(),
-    (t.ruid = "bundler=rspack@1.1.8");
-  var n = t.O(void 0, ["219", "181"], function () {
-    return t("59799");
+// Explain(() => {
+//   var e = {
+//       59799: function (e, r, t) {
+//         t(9461),
+//           t(27624),
+//           t(30286),
+//           t(8334),
+//           t(12338),
+//           t(93695),
+//           t(60322),
+//           t(40941),
+//           t(65134),
+//           t(27527),
+//           t(41655),
+//           t(64054),
+//           t(67103);
+//       },
+//     },
+//     r = {};
+//   function t(n) {
+//     var o = r[n];
+//     if (void 0 !== o) return o.exports;
+//     var u = (r[n] = { id: n, loaded: !1, exports: {} });
+//     return e[n].call(u.exports, u, u.exports, t), (u.loaded = !0), u.exports;
+//   }
+//   (t.m = e),
+//     (() => {
+//       var e,
+//         r = Object.getPrototypeOf
+//           ? function (e) {
+//               return Object.getPrototypeOf(e);
+//             }
+//           : function (e) {
+//               return e.__proto__;
+//             };
+//       t.t = function (n, o) {
+//         if (
+//           (1 & o && (n = this(n)),
+//           8 & o ||
+//             ("object" == typeof n &&
+//               n &&
+//               ((4 & o && n.__esModule) ||
+//                 (16 & o && "function" == typeof n.then))))
+//         )
+//           return n;
+//         var u = Object.create(null);
+//         t.r(u);
+//         var i = {};
+//         e = e || [null, r({}), r([]), r(r)];
+//         for (
+//           var f = 2 & o && n;
+//           "object" == typeof f && !~e.indexOf(f);
+//           f = r(f)
+//         )
+//           Object.getOwnPropertyNames(f).forEach(function (e) {
+//             i[e] = function () {
+//               return n[e];
+//             };
+//           });
+//         return (
+//           (i.default = function () {
+//             return n;
+//           }),
+//           t.d(u, i),
+//           u
+//         );
+//       };
+//     })(),
+//     (t.d = function (e, r) {
+//       for (var n in r)
+//         t.o(r, n) &&
+//           !t.o(e, n) &&
+//           Object.defineProperty(e, n, { enumerable: !0, get: r[n] });
+//     }),
+//     (t.hmd = function (e) {
+//       return (
+//         !(e = Object.create(e)).children && (e.children = []),
+//         Object.defineProperty(e, "exports", {
+//           enumerable: !0,
+//           set: function () {
+//             throw Error(
+//               "ES Modules may not assign module.exports or exports.*, Use ESM export syntax, instead: " +
+//                 e.id
+//             );
+//           },
+//         }),
+//         e
+//       );
+//     }),
+//     (t.g = (function () {
+//       if ("object" == typeof globalThis) return globalThis;
+//       try {
+//         return this || Function("return this")();
+//       } catch (e) {
+//         if ("object" == typeof window) return window;
+//       }
+//     })()),
+//     (t.o = function (e, r) {
+//       return Object.prototype.hasOwnProperty.call(e, r);
+//     }),
+//     (t.r = function (e) {
+//       "undefined" != typeof Symbol &&
+//         Symbol.toStringTag &&
+//         Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }),
+//         Object.defineProperty(e, "__esModule", { value: !0 });
+//     }),
+//     (t.nmd = function (e) {
+//       return (e.paths = []), !e.children && (e.children = []), e;
+//     }),
+//     (() => {
+//       var e = [];
+//       t.O = function (r, n, o, u) {
+//         if (n) {
+//           u = u || 0;
+//           for (var i = e.length; i > 0 && e[i - 1][2] > u; i--) e[i] = e[i - 1];
+//           e[i] = [n, o, u];
+//           return;
+//         }
+//         for (var f = 1 / 0, i = 0; i < e.length; i++) {
+//           for (
+//             var n = e[i][0], o = e[i][1], u = e[i][2], c = !0, a = 0;
+//             a < n.length;
+//             a++
+//           )
+//             (!1 & u || f >= u) &&
+//             Object.keys(t.O).every(function (e) {
+//               return t.O[e](n[a]);
+//             })
+//               ? n.splice(a--, 1)
+//               : ((c = !1), u < f && (f = u));
+//           if (c) {
+//             e.splice(i--, 1);
+//             var l = o();
+//             void 0 !== l && (r = l);
+//           }
+//         }
+//         return r;
+//       };
+//     })(),
+//     (t.rv = function () {
+//       return "1.1.8";
+//     }),
+//     (() => {
+//       var e = { 466: 0 };
+//       t.O.j = function (r) {
+//         return 0 === e[r];
+//       };
+//       var r = function (r, n) {
+//           var o = n[0],
+//             u = n[1],
+//             i = n[2],
+//             f,
+//             c,
+//             a = 0;
+//           if (
+//             o.some(function (r) {
+//               return 0 !== e[r];
+//             })
+//           ) {
+//             for (f in u) t.o(u, f) && (t.m[f] = u[f]);
+//             if (i) var l = i(t);
+//           }
+//           for (r && r(n); a < o.length; a++)
+//             (c = o[a]), t.o(e, c) && e[c] && e[c][0](), (e[c] = 0);
+//           return t.O(l);
+//         },
+//         n = (self.webpackChunk = self.webpackChunk || []);
+//       n.forEach(r.bind(null, 0)), (n.push = r.bind(null, n.push.bind(n)));
+//     })(),
+//     (t.ruid = "bundler=rspack@1.1.8");
+//   var n = t.O(void 0, ["219", "181"], function () {
+//     return t("59799");
+//   });
+//   n = t.O(n);
+// })();
+// Explain;
+
+const backendBrandsMock = [
+  "Andera",
+  "Ardell",
+  "Armada",
+  "Avene",
+  "Batiste",
+  "Banana Boat",
+  "Bioderma",
+  "Bobini",
+  "Burberry",
+  "Cabana Sun",
+  "Calvin Klein",
+  "Calypso",
+  "Carroten",
+  "Cetaphil",
+  "Cliven Italy",
+  "Cocoa Skin Therapy",
+  "Colour Me",
+  "Cristal Momento",
+  "Creme of Nature",
+  "DAX",
+  "Deomania",
+  "Dimples",
+  "Disaar",
+  "Dove",
+  "Duo",
+  "Eco Tools",
+  "Enercos",
+  "Energy Hair Food",
+  "Essence",
+  "FashKool",
+  "Frais",
+  "Garnier",
+  "Galeria",
+  "Gillette",
+  "Hair Burst",
+  "Hawaiian Tropic",
+  "Huda Beauty",
+  "IBD",
+  "Impress New",
+  "Kashmir",
+  "Karsell",
+  "Kativa",
+  "Kiss",
+  "Kodomo",
+  "Lady’s Secret",
+  "Lilien",
+  "L'Oréal",
+  "L`Origin",
+  "Milton-Lloyd",
+  "Modish",
+  "Momento",
+  "Moroccanoil",
+  "Morfose",
+  "Naturalis",
+  "Nature’s Bounty",
+  "Nivea",
+  "Ohoud",
+  "Olaplex",
+  "Oriense",
+  "ORS",
+  "Pantene",
+  "Perfumer’s Choice",
+  "Play Girl",
+  "Playtex",
+  "Ravita",
+  "Re-Gen",
+  "Revox",
+  "Revox Plex",
+  "Revlon",
+  "RT Logos Stacked",
+  "Salon Perfect",
+  "Schick",
+  "SG",
+  "SGX",
+  "Selsium",
+  "Silkia",
+  "Smart Collection",
+  "Starkin",
+  "Stoply",
+  "Thicker Fuller Hair",
+  "Uniq One",
+  "Union Cosmetic",
+  "Vichy",
+  "WetBrush",
+  "Wildcolor",
+  "XL",
+  "Yara",
+  "YM",
+  "YouthHair",
+  "Zact",
+  "Zero Frizz"
+];
+
+function mapBrandsByLetter(brandsArray) {
+  const result = {};
+
+  brandsArray.forEach(brand => {
+    if (!brand || typeof brand !== "string") return;
+
+    const letter = brand[0].toUpperCase();
+
+    if (!result[letter]) {
+      result[letter] = [];
+    }
+
+    result[letter].push(brand);
   });
-  n = t.O(n);
-})();
-Explain;
+
+  return { brands: result };
+}
+
+// function mapBrandsByLetter(brandsArray) {
+//   const result = {};
+
+//   brandsArray.forEach(brand => {
+//     if (!brand || typeof brand !== "string") return;
+
+//     const cleanBrand = brand.trim();
+//     if (!cleanBrand) return;
+
+//     const letter = cleanBrand[0].toUpperCase();
+
+//     if (!result[letter]) {
+//       result[letter] = [];
+//     }
+
+//     result[letter].push(cleanBrand);
+//   });
+
+//   // sort brands inside each letter
+//   Object.keys(result).forEach(letter => {
+//     result[letter].sort((a, b) => a.localeCompare(b));
+//   });
+
+//   return { brands: result };
+// }
+
+function renderBrands(data, containerId) {
+  const container = document.getElementById(containerId);
+  container.innerHTML = "";
+
+  Object.keys(data).forEach(letter => {
+    const column = document.createElement("div");
+    column.className = "mega-column";
+
+    const title = document.createElement("h4");
+    title.textContent = letter;
+
+    column.appendChild(title);
+
+    data[letter].forEach(brand => {
+      const link = document.createElement("a");
+      link.href = `/brands/${brand.replace(/\s+/g, "-").toLowerCase()}`;
+      link.textContent = brand;
+
+      column.appendChild(link);
+    });
+
+    container.appendChild(column);
+  });
+}
+
+// function renderBrands(data, containerId) {
+//   const container = document.getElementById(containerId);
+//   if (!container) return;
+
+//   container.innerHTML = "";
+
+//   Object.keys(data)
+//     .sort()
+//     .forEach(letter => {
+//       const column = document.createElement("div");
+//       column.className = "mega-column";
+
+//       const title = document.createElement("h4");
+//       title.textContent = letter;
+//       column.appendChild(title);
+
+//       data[letter].forEach(brand => {
+//         const link = document.createElement("a");
+
+//         link.href = `/brands/${brand
+//           .toLowerCase()
+//           .replace(/[^a-z0-9]+/g, "-")
+//           .replace(/^-|-$/g, "")}`;
+
+//         link.textContent = brand;
+//         column.appendChild(link);
+//       });
+
+//       container.appendChild(column);
+//     });
+// }
+
+const brandsData = mapBrandsByLetter(backendBrandsMock);
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(brandsData);
+  renderBrands(brandsData.brands, "brandsContainer");
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const brandNav = document.querySelector('.nav-item[data-target="brands"]');
+  if (brandNav) {
+    brandNav.addEventListener('click', () => {
+      const menu = document.querySelector('#brands');
+      if (menu) menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+    });
+  }
+});
